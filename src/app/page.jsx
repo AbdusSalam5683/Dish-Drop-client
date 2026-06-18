01c0bf347bd13bbb19e1519de730e9e8f591175f
Abdus-Salam-814 <abdussalam6111997@gmail.com>
2026-06-18 18:18:31 +0600

    Add favicon.svg and remove test error from page component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,7 @@ import DishDropLogo from '@/components/Logo';
 import React from 'react';
 
 const page = () => {
-   throw new Error("This is a test error!");
+   
     return (
         <div>
             <h1>This is a simple page</h1>
