597ca59e359a3eac1f7a7629b1184418dd3203e8
Abdus-Salam-814 <abdussalam6111997@gmail.com>
2026-06-18 14:57:15 +0600

    i create navbar and footer

---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+import DishDropLogo from '@/component/Logo';
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <h1>This is a simple page</h1>
+            
+        </div>
+    );
+};
+
+export default page;
